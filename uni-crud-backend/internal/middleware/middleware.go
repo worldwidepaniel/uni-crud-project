83cfddc8d1c9c41528dcf2b69d0ee2ba4a35a604
worldwidepaniel <dan.porzucek@gmail.com>
2022-12-17 11:20:18 +0100

    demand bearer prefix

--- uni-crud-backend/internal/middleware/middleware.go
+++ uni-crud-backend/internal/middleware/middleware.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/worldwidepaniel/uni-crud-project/internal/interfaces"
@@ -10,6 +11,13 @@ import (
 func JWTAuth(secret []byte, jwtUtils interfaces.JWTUtils) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		jwtToken := ctx.Request.Header.Get("token")
+		if !strings.Contains(jwtToken, "Bearer ") {
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "invalid token",
+			})
+			ctx.Abort()
+		}
+		jwtToken = strings.TrimPrefix(jwtToken, "Bearer ")
 		if err := jwtUtils.ValidateJWT(jwtToken, secret); err != nil {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
 				"error": "invalid token",
